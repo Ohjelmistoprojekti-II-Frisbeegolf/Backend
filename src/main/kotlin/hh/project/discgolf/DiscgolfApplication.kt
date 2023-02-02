458ba64c0ed094f6ecb020de23ccb38f478e18c2
Jesse Plym <bgu254@myy.haaga-helia.fi>
2023-02-02 12:41:53 +0200

    initial commit

---
+++ src/main/kotlin/hh/project/discgolf/DiscgolfApplication.kt
@@ -0,0 +1,11 @@
+package hh.project.discgolf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiscgolfApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiscgolfApplication>(*args)
+}
---
+++ src/test/kotlin/hh/project/discgolf/DiscgolfApplicationTests.kt
@@ -0,0 +1,13 @@
+package hh.project.discgolf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscgolfApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
