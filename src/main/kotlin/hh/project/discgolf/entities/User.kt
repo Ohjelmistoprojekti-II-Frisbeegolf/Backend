3b149ac84b5ebcb25b1a5c38c9db54eba6d1f32e
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-02-06 13:22:48 +0200

    Added attributes for user entity

--- src/main/kotlin/hh/project/discgolf/entities/User.kt
+++ src/main/kotlin/hh/project/discgolf/entities/User.kt
@@ -1,4 +1,19 @@
 package hh.project.discgolf.entities
 
-class User {
-}
+import hh.project.discgolf.enums.UserRole
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class User(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var userId: Long = -1,
+    @Column(nullable = false, unique = true)
+    var username: String = "",
+    var email: String = "",
+    var password: String = "",
+    var role: UserRole? = null
+)
---
+++ src/main/kotlin/hh/project/discgolf/enums/UserRole.kt
@@ -0,0 +1,4 @@
+package hh.project.discgolf.enums
+
+enum class UserRole {
+}
