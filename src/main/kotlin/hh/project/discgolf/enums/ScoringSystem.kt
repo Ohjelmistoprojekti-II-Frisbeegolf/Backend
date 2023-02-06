d5bdd37bef3ec68c2c0b0360fc17f11a52cfb07d
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-02-06 13:41:02 +0200

    Enum type for scoring system

---
+++ src/main/kotlin/hh/project/discgolf/enums/ScoringSystem.kt
@@ -0,0 +1,13 @@
+package hh.project.discgolf.enums
+
+// Can be found from https://discgolfunited.com/blog/post/dictionary-rules-and-scoring
+enum class ScoringSystem {
+    PAR,
+    BIRDIE,
+    EAGLE,
+    ALBATROSS,
+    ACE,
+    BOGEY,
+    DOUBLE_BOGEY,
+    TRIPLE_BOGEY
+}
