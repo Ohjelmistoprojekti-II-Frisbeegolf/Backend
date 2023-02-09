502a472946d771740b7e60264a06f73510190aa6
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-02-09 17:41:16 +0200

    Removed annotation

--- src/main/kotlin/hh/project/discgolf/repositories/GameRepository.kt
+++ src/main/kotlin/hh/project/discgolf/repositories/GameRepository.kt
@@ -3,9 +3,7 @@ package hh.project.discgolf.repositories
 import hh.project.discgolf.entities.Game
 import hh.project.discgolf.entities.User
 import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface GameRepository : CrudRepository<Game, Long> {
     fun findByUser(user: User): Game?
 }
