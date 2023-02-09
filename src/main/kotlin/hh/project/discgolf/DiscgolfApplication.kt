a1682d92d8ec31053d06efb03615d0d9207b06f1
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-02-09 18:55:33 +0200

    Testdata is done

--- src/main/kotlin/hh/project/discgolf/DiscgolfApplication.kt
+++ src/main/kotlin/hh/project/discgolf/DiscgolfApplication.kt
@@ -20,25 +20,31 @@ class DiscgolfApplication {
 		val maija = userRepo.save(User(username = "Maija", email = "maijakas@hotmail.com", password = "olenmaija", role = UserRole.USER))
 		val admin = userRepo.save(User(username = "admin", email = "admin@discgolf.com", password = "admin", role = UserRole.ADMIN))
 
+
+
 		// Courses for testdata
 		val puolarmaari = courseRepo.save(Course(courseName = "Puolarmaari", courseStreetaddress = "Puolarmaari 3", coursePostalcode = "02210", courseTown = "Espoo"))
 		val oittaaKalliometsa = courseRepo.save(Course(courseName = "Oittaa Kalliometsä", courseStreetaddress = "Oittaantie", coursePostalcode = "02740", courseTown = "Espoo"))
 		val tali = courseRepo.save(Course(courseName = "Talin frisbeegolfpuisto", courseStreetaddress = "Takkatie 36", coursePostalcode = "00370", courseTown = "Helsinki"))
 
+
+
 		// Holes for Puolarmaari
 		for (hole in 1..20) {
-			holeRepo.save(Hole(course = puolarmaari, holeLength = 50, holeNumber = hole, holePar = 3))
+			holeRepo.save(Hole(course = puolarmaari, holeLength = (30..120).random(), holeNumber = hole, holePar = 3))
 		}
 
 		// Holes for Oittaan Kalliometsä
 		for (hole in 1..18) {
-			holeRepo.save(Hole(course = oittaaKalliometsa, holeLength = 75, holeNumber = hole, holePar = 3))
+			holeRepo.save(Hole(course = oittaaKalliometsa, holeLength = (30..120).random(), holeNumber = hole, holePar = 3))
 		}
 
 		// Holes for Tali
 		for (hole in 1..18) {
-			holeRepo.save(Hole(course = tali, holeLength = 90, holeNumber = hole, holePar = 5))
+			holeRepo.save(Hole(course = tali, holeLength = (30..120).random(), holeNumber = hole, holePar = 5))
 		}
+
+
 
 		val gameAtPuolarmaari = gameRepo.save(Game(user = keijo, course = puolarmaari, steps = 5000, startingDatetime = LocalDateTime.now(),
 			endingDatetime = LocalDateTime.now().plusMinutes(90)))
@@ -50,6 +56,28 @@ class DiscgolfApplication {
 			endingDatetime = LocalDateTime.now().plusMinutes(200)))
 
 
+
+		// Strokes for game at Puolarmaari
+		for (hole: Hole in gameAtPuolarmaari.course!!.holes) {
+			strokeRepo.save(Stroke(hole = hole, game = gameAtPuolarmaari, score = (1..10).random()))
+		}
+
+
+
+		// Strokes for game at Oittaan Kalliometsä
+		for (hole: Hole in gameAtOittaaKalliometsa.course!!.holes) {
+			strokeRepo.save(Stroke(hole = hole, game = gameAtOittaaKalliometsa, score = (1..10).random()))
+		}
+
+
+
+		// Strokes for game at Tali.
+		for (hole: Hole in gameAtTali.course!!.holes) {
+			strokeRepo.save(Stroke(hole = hole, game = gameAtTali, score = (1..10).random()))
+
+
+
+		}
 	}
 
 }
