43d6d5ee5aee0002d099b3286b1783c784cc0e7c
unofficialnikke <nicklasnystrm3@gmail.com>
2023-02-09 12:58:31 +0200

    created entity for stroke

--- src/main/kotlin/hh/project/discgolf/entities/Stroke.kt
+++ src/main/kotlin/hh/project/discgolf/entities/Stroke.kt
@@ -1,4 +1,23 @@
 package hh.project.discgolf.entities
 
-class Stroke {
-}
+import jakarta.persistence.*
+import lombok.Getter
+import lombok.Setter
+import lombok.ToString
+
+@Entity
+@Getter
+@Setter
+@ToString
+class Stroke (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    var strokeId: Long = -1L,
+
+    var hole: Hole? = null,
+
+    var score: Number = 0,
+
+    @ManyToOne
+    var game: Game? = null,
+)
