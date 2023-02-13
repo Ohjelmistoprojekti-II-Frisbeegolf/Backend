f0c54a0340fdc72ffdc7eb652ad13764569d3f8d
Jesse Plym <bgu254@myy.haaga-helia.fi>
2023-02-13 14:52:44 +0200

    user controller get single user

--- src/main/kotlin/hh/project/discgolf/controllers/UserController.kt
+++ src/main/kotlin/hh/project/discgolf/controllers/UserController.kt
@@ -4,6 +4,7 @@ import hh.project.discgolf.entities.User
 import hh.project.discgolf.services.UserService
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -13,4 +14,8 @@ class UserController(private val userService: UserService) {
 
     @GetMapping(value = ["/users"])
     fun getAllUsers(): List<User> = userService.getAllUsers()
+
+    @GetMapping(value = ["/users/{id}"])
+    fun getUserById(@PathVariable("id") id: Long): User =
+        userService.getUserById(id)
 }
--- src/main/kotlin/hh/project/discgolf/services/UserService.kt
+++ src/main/kotlin/hh/project/discgolf/services/UserService.kt
@@ -2,10 +2,38 @@ package hh.project.discgolf.services
 
 import hh.project.discgolf.entities.User
 import hh.project.discgolf.repositories.UserRepository
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
 import org.springframework.stereotype.Service
 
 @Service
 class UserService (private val userRepository: UserRepository){
 
     fun getAllUsers(): List<User> = userRepository.findAll()
+
+    fun getUserById(id: Long): User =
+        userRepository.findById(id)
+            .orElseThrow()
+
+    fun createNewUser(user: User): User = userRepository.save(user)
+
+    fun deleteUser(id: Long) {
+        return if (userRepository.existsById(id)) {
+            userRepository.deleteById(id)
+        } else throw NotFoundException()
+    }
+
+    fun updateUser(id: Long, user: User): User {
+        return if (userRepository.existsById(id)) {
+            userRepository.save(
+                User(
+                    userId = id,
+                    games = user.games,
+                    username = user.username,
+                    email = user.email,
+                    password = user.password,
+                    role = user.role
+                )
+            )
+        } else throw NotFoundException()
+    }
 }
