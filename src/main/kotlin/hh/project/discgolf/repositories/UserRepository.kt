fe155b1652fe803b50af6913a47c2fd0f3ea97f6
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-02-13 20:46:31 +0200

    Function for finding user by username

--- src/main/kotlin/hh/project/discgolf/repositories/UserRepository.kt
+++ src/main/kotlin/hh/project/discgolf/repositories/UserRepository.kt
@@ -4,4 +4,6 @@ import hh.project.discgolf.entities.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Long> {
+
+    fun findByUsername(username : String) : User
 }
