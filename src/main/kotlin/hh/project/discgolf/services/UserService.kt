b98ce91dadb1ad4668a645ea99448aaeebb17b3a
Jesse Plym <bgu254@myy.haaga-helia.fi>
2023-02-13 12:56:41 +0200

    user service and controller

--- src/main/kotlin/hh/project/discgolf/services/UserService.kt
+++ src/main/kotlin/hh/project/discgolf/services/UserService.kt
@@ -1,7 +1,9 @@
 package hh.project.discgolf.services
 
+import hh.project.discgolf.repositories.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
-class UserService {
+class UserService (private val userRepository: UserRepository){
+
 }
