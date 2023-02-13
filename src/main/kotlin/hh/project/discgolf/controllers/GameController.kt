faf38264b0577a35afc68289a0bed23b58739610
unofficialnikke <nicklasnystrm3@gmail.com>
2023-02-13 15:36:11 +0200

    added endpoint for find game by id and create new game

--- src/main/kotlin/hh/project/discgolf/controllers/GameController.kt
+++ src/main/kotlin/hh/project/discgolf/controllers/GameController.kt
@@ -4,6 +4,9 @@ import hh.project.discgolf.entities.Game
 import hh.project.discgolf.services.GameService
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,4 +15,11 @@ class GameController(private val gameService: GameService) {
 
     @GetMapping(value = ["/games"])
     fun getAllGames(): List<Game> = gameService.getAllGames()
+
+    @GetMapping(value = ["/games/{id}"])
+    fun getGameById(@PathVariable("id") gameId: Long): Game =
+        gameService.getGameById(gameId)
+
+    @PostMapping(value = ["/games"])
+    fun createGame(@RequestBody payload: Game): Game = gameService.createGame(payload)
 }
--- src/main/kotlin/hh/project/discgolf/services/GameService.kt
+++ src/main/kotlin/hh/project/discgolf/services/GameService.kt
@@ -2,10 +2,19 @@ package hh.project.discgolf.services
 
 import hh.project.discgolf.entities.Game
 import hh.project.discgolf.repositories.GameRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class GameService(private val gameRepository: GameRepository) {
 
     fun getAllGames(): List<Game> = gameRepository.findAll()
+
+    fun getGameById(gameId: Long): Game = gameRepository.findById(gameId)
+        .orElseThrow()
+
+    fun createGame(game: Game): Game = gameRepository.save(game)
+
+    
 }
