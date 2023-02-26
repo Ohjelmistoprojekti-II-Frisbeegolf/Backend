7acd8321d89945f420030e9a625708840943298b
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-02-26 15:05:41 +0200

    findByCourseName return Course or null

--- src/main/kotlin/hh/project/discgolf/repositories/CourseRepository.kt
+++ src/main/kotlin/hh/project/discgolf/repositories/CourseRepository.kt
@@ -2,9 +2,10 @@ package hh.project.discgolf.repositories
 
 import hh.project.discgolf.entities.Course
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface CourseRepository : JpaRepository<Course, Long> {
 
-    fun findByCourseName(courseName : String) : Course
+    fun findByCourseName(courseName : String) : Course? = null
 
 }
