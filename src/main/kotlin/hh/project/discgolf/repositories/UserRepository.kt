30ae33ae87e17e478442a6aee2161644de11ed10
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-02-27 12:15:56 +0200

    Function for getting the data from db about how many games user has played

--- src/main/kotlin/hh/project/discgolf/repositories/UserRepository.kt
+++ src/main/kotlin/hh/project/discgolf/repositories/UserRepository.kt
@@ -2,8 +2,12 @@ package hh.project.discgolf.repositories
 
 import hh.project.discgolf.entities.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository : JpaRepository<User, Long> {
 
     fun findByUsername(username : String) : User
+
+    @Query("SELECT COUNT(user_id) FROM Game WHERE user_id = :userId", nativeQuery = true)
+    fun gameAmountForUser(userId : Long) : Int
 }
