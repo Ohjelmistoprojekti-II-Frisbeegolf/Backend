b00c68338878f79b1074f77e6744b9d82fd90e02
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-03-01 18:28:11 +0200

    Added value for scores over triple bogey

--- src/main/kotlin/hh/project/discgolf/enums/ScoringSystem.kt
+++ src/main/kotlin/hh/project/discgolf/enums/ScoringSystem.kt
@@ -3,11 +3,12 @@ package hh.project.discgolf.enums
 // Can be found from https://discgolfunited.com/blog/post/dictionary-rules-and-scoring
 enum class ScoringSystem {
     PAR,
+    ACE,
     BIRDIE,
     EAGLE,
     ALBATROSS,
-    ACE,
     BOGEY,
     DOUBLE_BOGEY,
-    TRIPLE_BOGEY
+    TRIPLE_BOGEY,
+    OVER_TRIPLE_BOGEY
 }
