218b1ea20575a6d5aed8a0fdfd71ea4cb917236f
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-03-01 14:06:28 +0200

    Function and query for finding out how many throws user has thrown

--- src/main/kotlin/hh/project/discgolf/repositories/UserRepository.kt
+++ src/main/kotlin/hh/project/discgolf/repositories/UserRepository.kt
@@ -10,4 +10,13 @@ interface UserRepository : JpaRepository<User, Long> {
 
     @Query("SELECT EXTRACT(EPOCH FROM (ending_datetime - starting_datetime)) FROM Game WHERE user_id = :userId", nativeQuery = true)
     fun totalTimePlayed(userId : Long) : List<Long>
+
+    @Query("SELECT SUM(s.score)" +
+            "FROM STROKE s " +
+            "JOIN GAME g on s.game_id = g.game_id " +
+            "JOIN USERS u on g.user_id = u.user_id " +
+            "WHERE u.user_id = :userId",
+            nativeQuery = true
+    )
+    fun getTotalThrowsThrown(userId : Long) : Int? = 0
 }
