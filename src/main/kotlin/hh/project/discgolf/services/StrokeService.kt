4bdee9270c40630456d66d2b311e5e5981c765d7
Jesse Plym <bgu254@myy.haaga-helia.fi>
2023-03-02 13:26:29 +0200

    stroke service and controller made

---
+++ src/main/kotlin/hh/project/discgolf/services/StrokeService.kt
@@ -0,0 +1,17 @@
+package hh.project.discgolf.services
+
+import hh.project.discgolf.entities.Stroke
+import hh.project.discgolf.repositories.StrokeRepository
+import org.springframework.stereotype.Service
+
+@Service
+class StrokeService (private val strokeRepository: StrokeRepository) {
+
+    fun getAllStrokes(): List<Stroke> = strokeRepository.findAll()
+
+    fun getSingleStrokeById(strokeId: Long): Stroke =
+        strokeRepository.findById(strokeId)
+            .orElseThrow { NoSuchElementException("Stroke with given id doesn't exist!") }
+
+    fun createNewStroke(stroke: Stroke): Stroke = strokeRepository.save(stroke)
+}
