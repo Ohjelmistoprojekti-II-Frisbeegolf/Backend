936b10fcc941f933f8a544f130f9e503c07566ef
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-03-06 14:19:48 +0200

    Service for weather

--- src/main/kotlin/hh/project/discgolf/services/WeatherService.kt
+++ src/main/kotlin/hh/project/discgolf/services/WeatherService.kt
@@ -1,4 +1,11 @@
 package hh.project.discgolf.services
 
+import org.springframework.stereotype.Service
+
+@Service
 class WeatherService {
+
+    fun fetchWeather(lon : Double, lat : Double) : String {
+        return "$lon $lat"
+    }
 }
