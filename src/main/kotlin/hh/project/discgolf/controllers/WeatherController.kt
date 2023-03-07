9661ccb4686c36f48e66be69b2976a936dc9a081
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-03-07 13:54:19 +0200

    Added try-catch

--- src/main/kotlin/hh/project/discgolf/controllers/WeatherController.kt
+++ src/main/kotlin/hh/project/discgolf/controllers/WeatherController.kt
@@ -1,21 +1,31 @@
 package hh.project.discgolf.controllers
 
 import hh.project.discgolf.services.WeatherService
+import io.swagger.v3.oas.annotations.Hidden
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.client.ClientHttpRequest
+import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import kotlin.jvm.Throws
 
 @RestController
 @CrossOrigin
+@Hidden
 class WeatherController(private val weatherService: WeatherService) {
 
-    @GetMapping(value = ["/weather/lon={lon}/lat={lat}"])
-    fun getWeather(@PathVariable("lon") lon : Double, @PathVariable("lat") lat : Double ) = weatherService.handleWeather(lon, lat)
+    @GetMapping(value = ["/weather"])
+    fun getWeather(@RequestParam("lon") lon : String, @RequestParam("lat") lat : String ): WeatherService.WeatherObject? {
+        return try {
+            weatherService.handleWeather(lon, lat)
+        } catch ( e : MissingServletRequestParameterException) {
+            throw MissingServletRequestParameterException(e.parameterName, "")
+        }
 
-
+    }
 }
