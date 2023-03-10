5606f9a7c0f5db2e20c0a58a30d35a5a62445a7a
unofficialnikke <nicklasnystrm3@gmail.com>
2023-03-10 12:33:39 +0200

    new tests & simplified the code

--- src/test/kotlin/hh/project/discgolf/repositories/UserRepositoryTests.kt
+++ src/test/kotlin/hh/project/discgolf/repositories/UserRepositoryTests.kt
@@ -1,8 +1,7 @@
 package hh.project.discgolf.repositories
 
-import hh.project.discgolf.entities.Game
+import hh.project.discgolf.entities.*
 import org.assertj.core.api.Assertions.*
-import hh.project.discgolf.entities.User
 import hh.project.discgolf.repositories.UserRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -18,24 +17,24 @@ import java.time.LocalDateTime
 @ActiveProfiles("test")
 class UserRepositoryTests @Autowired constructor(
     val userRepository: UserRepository,
-    val gameRepository: GameRepository
+    val gameRepository: GameRepository,
+    val strokeRepository: StrokeRepository,
+    val holeRepository: HoleRepository,
+    val courseRepository: CourseRepository
 ) {
-    val user1 = User(username = "user1", password = "password", email = "user1@gmail.com")
-    val user2 = User(username = "user2", password = "password", email = "user2@hotmail.com")
-    val user3 = User(username = "user3", password = "password", email = "user3@luukku.fi")
 
     @BeforeEach
     fun init() {
         userRepository.deleteAll()
         val user1 = User(userId = 1L, username = "Matti", email = "matti@mail.com", password = "df68327ghj")
         val user2 = User(userId = 2L, username = "Maija", email = "maija@mail.com", password = "jdncs788ds")
-        userRepository.saveAll(listOf(user1, user2))
+        val user3 = User(userId = 3L, username = "user3", email = "user3@mail.com", password = "password")
+        userRepository.saveAll(listOf(user1, user2, user3))
     }
 
     @Test
     fun `should save a user`() {
-        val newUser = User(userId = 3L, username = "Ville", email = "v@mail.com", password = "hjasasjf23")
-        userRepository.save(newUser)
+        val savedUser = userRepository.findByUsername("Maija")
         assertThat(userRepository.findById(3L)).isNotNull
     }
 
@@ -43,7 +42,7 @@ class UserRepositoryTests @Autowired constructor(
     fun `should return all users`() {
         val users = userRepository.findAll()
         assertThat(users).isNotEmpty
-        assertThat(users).hasSize(2)
+        assertThat(users).hasSize(3)
     }
 
     @Test
@@ -69,16 +68,14 @@ class UserRepositoryTests @Autowired constructor(
 
     @Test
     fun`user haves zero games - DB should return null - result should be zero`() {
-        val user = User(username = "user1", password = "password", email = "email1@email.com")
-        val savedUser = userRepository.save(user)
+        val savedUser = userRepository.findByUsername("Maija")
         assertThat(userRepository.getStepsForUser(savedUser.userId)).isNull()
         assertThat(userRepository.getStepsForUser(savedUser.userId)?:0).isEqualTo(0)
     }
 
     @Test
     fun`user haves one game with 222 steps - should return 222`() {
-        val user = User(username = "user2", password = "password", email = "email2@email.com")
-        val savedUser = userRepository.save(user)
+        val savedUser = userRepository.findByUsername("Matti")
         val game = Game(steps = 222, user = savedUser)
         gameRepository.save(game)
         assertThat(userRepository.getStepsForUser(savedUser.userId)).isEqualTo(222)
@@ -86,8 +83,7 @@ class UserRepositoryTests @Autowired constructor(
 
     @Test
     fun`user haves two games, one with 222 steps, and one with 8 000 steps - should return 8 222`() {
-        val user = User(username = "user3", password = "password", email = "email3@email.com")
-        val savedUser = userRepository.save(user)
+        val savedUser = userRepository.findByUsername("Maija")
         val game1 = Game(steps = 222, user = savedUser)
         val game2 = Game(steps = 8_000, user = savedUser)
         gameRepository.save(game1)
@@ -96,37 +92,45 @@ class UserRepositoryTests @Autowired constructor(
     }
     @Test
     fun `user has played a game of 2 hours - should return 7 200 seconds`() {
-        val saveUser = userRepository.save(user1)
+        val savedUser = userRepository.findByUsername("Maija")
         val newGame = Game(
             startingDatetime = LocalDateTime.now(),
             endingDatetime = LocalDateTime.now().plusMinutes(120),
-            user = saveUser)
+            user = savedUser)
         gameRepository.save(newGame)
-        assertThat(userRepository.totalTimePlayed(saveUser.userId)).isEqualTo(7_200L)
+        assertThat(userRepository.totalTimePlayed(savedUser.userId)).isEqualTo(7_200L)
     }
     @Test
     fun `user has played two games, total of 2,5 hours - should return 9 000 seconds`() {
-        val saveUser = userRepository.save(user1)
+        val savedUser = userRepository.findByUsername("Matti")
         val game1 = Game(
             startingDatetime =  LocalDateTime.now(),
             endingDatetime = LocalDateTime.now().plusMinutes(60),
-            user = saveUser
+            user = savedUser
         )
         val game2 = Game(
             startingDatetime =  LocalDateTime.now(),
             endingDatetime = LocalDateTime.now().plusMinutes(90),
-            user = saveUser
+            user = savedUser
         )
         gameRepository.saveAll(listOf(game1, game2))
-        assertThat(userRepository.totalTimePlayed(saveUser.userId)).isEqualTo(9_000L)
+        assertThat(userRepository.totalTimePlayed(savedUser.userId)).isEqualTo(9_000L)
     }
     @Test
     fun `user has played no games, should return null`() {
-        val saveUser = userRepository.save(user3)
-        assertThat(userRepository.totalTimePlayed(saveUser.userId)).isEqualTo(null)
+        val savedUser = userRepository.findByUsername("Maija")
+        assertThat(userRepository.totalTimePlayed(savedUser.userId)).isEqualTo(null)
     }
+    @Test
+    fun `user has played a game with one stroke - should return 1`() {
+        val savedUser = userRepository.findByUsername("user3")
+        val newGame = Game(user = savedUser)
+        val savedGame = gameRepository.save(newGame)
+        val newStroke = Stroke(game = savedGame)
+        strokeRepository.save(newStroke)
 
-
+        assertThat(userRepository.getTotalThrowsThrown(savedUser.userId)).isEqualTo(1)
+    }
 }
 
 
