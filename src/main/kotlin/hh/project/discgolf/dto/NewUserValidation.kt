d3842c6f3bc1e4a798bd8267c5cee681b2017deb
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-03-27 16:16:51 +0300

    Fixed validation

--- src/main/kotlin/hh/project/discgolf/dto/NewUserValidation.kt
+++ src/main/kotlin/hh/project/discgolf/dto/NewUserValidation.kt
@@ -7,16 +7,16 @@ import jakarta.validation.constraints.Size
 import kotlin.math.min
 
 data class NewUserValidation(
-    @NotBlank
-    @Size(min = 5, max = 20, message = "Username must be 5-20 characters long")
+    @field:NotBlank
+    @field:Size(min = 5, max = 20, message = "Username must be 5-20 characters long")
     val username : String,
 
-    @NotBlank
-    @Size(min = 7, max = 32, message = "Password must be 7-32 characters long")
+    @field:NotBlank
+    @field:Size(min = 7, max = 32, message = "Password must be 7-32 characters long")
     val password : String,
 
-    @NotBlank
-    @Size(min = 7, max = 32, message = "Password must be 7-32 characters long")
+    @field:NotBlank
+    @field:Size(min = 7, max = 32, message = "Password must be 7-32 characters long")
     val passwordCheck : String,
 
 ) {
