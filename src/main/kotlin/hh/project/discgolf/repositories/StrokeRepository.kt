e59a3f711d717c239a2a4a454dacd547c44766a1
Jesse Plym <bgu254@myy.haaga-helia.fi>
2023-04-27 11:41:26 +0300

    new stroke endpoint made

--- src/main/kotlin/hh/project/discgolf/repositories/StrokeRepository.kt
+++ src/main/kotlin/hh/project/discgolf/repositories/StrokeRepository.kt
@@ -1,7 +1,11 @@
 package hh.project.discgolf.repositories
 
+import hh.project.discgolf.entities.Game
 import hh.project.discgolf.entities.Stroke
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface StrokeRepository : JpaRepository<Stroke, Long> {
+
+    fun findByGame(game: Game): List<Stroke>
 }
